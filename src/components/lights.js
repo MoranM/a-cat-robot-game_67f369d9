785e6087ded291c7797fdef0e817c4bfefed2195
MoranMishan <mishanmo@gmail.com>
2025-04-07 09:00:46 +0300

    Add src/components/lights.js

---
+++ src/components/lights.js
@@ -0,0 +1,11 @@
+// lights.js
+import * as THREE from 'three';
+
+export function createLights(scene) {
+  const ambientLight = new THREE.AmbientLight(0x404040);
+  scene.add(ambientLight);
+
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+  directionalLight.position.set(1, 1, 1).normalize();
+  scene.add(directionalLight);
+}
