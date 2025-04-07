19b6dfcfd6be9aae5ad2f47251892716b3785304
MoranMishan <mishanmo@gmail.com>
2025-04-07 09:00:47 +0300

    Add src/components/renderer.js

---
+++ src/components/renderer.js
@@ -0,0 +1,9 @@
+// renderer.js
+import * as THREE from 'three';
+
+export function createRenderer() {
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+  return renderer;
+}
