cb2fccd6a38bd85b2b6eb2f1b3b21a54b06b435a
MoranMishan <mishanmo@gmail.com>
2025-04-07 09:00:48 +0300

    Add src/entities/player.js

---
+++ src/entities/player.js
@@ -0,0 +1,12 @@
+// player.js
+export class Player {
+  constructor() {
+    this.health = 100;
+    this.position = { x: 0, y: 0, z: 0 };
+    // Additional player setup
+  }
+
+  update() {
+    // Update player logic
+  }
+}
